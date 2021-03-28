0e6822f0bbee89d1212bc0b501c2da965d704fc0
AimlessAvenue <alessiobaudo17@gmail.com>
2021-03-28 04:30:05 +0200

    Create prompt fro input with validation, minor optimizations and fixex

--- resources/scripts.js
+++ resources/scripts.js
@@ -2,27 +2,45 @@ const OPTIONS = ["rock","paper","scissors"]
 const WIN = "You win! :D"
 const LOOSE = "You loose.."
 const TIE = "It's a tie! Get ready for the next round."
-let playerPlay = "RoCk"
+let playerPlay
 let playerScore=0
 let computerScore=0
-let check
 let roundCounter=0
+let check
+let playerValidated
 
-//Helper functions 
+commands();
+//Helper functions -------------------------
+//General 
+function commands(){
+    console.info("Welcome hooman. You can start a new game by typing \"game()\" in the console.")
+}
+function initialize(){
+    playerPlay=0;
+    computerScore=0;
+    roundCounter=0;
+} 
 function computerPlay() {
     return OPTIONS[Math.floor(Math.random()*OPTIONS.length)]
 }
+function playerValidate(playerPlay){
+    playerPlay= prompt("Choose your weapon! You can choose between \"Rock\", \"Paper\", \"Scissors\"")
+    while (!OPTIONS.includes(playerPlay.toLowerCase())){
+        playerPlay = prompt(`The \"${playerPlay}\" is a mighty forbidden weapon! Make sure to select either \"Rock\", \"Paper\" or \"Scissors\"`);
+    }
+    playerValidated=playerPlay.toLowerCase();
+    }
 //Add points to player or computer score.
 function scoreChecker() {
     if (check.includes(TIE)) {
-        console.log(`Round ${roundCounter}: ${TIE}`)
+        console.log(`Round ${roundCounter}: You both choosed ${TIE}`)
     }
     else if (check.includes(WIN)){
         console.log(`Round ${roundCounter}: Nice job with this round, ${WIN}`)
         playerScore++;
     }
     else if (check.includes(LOOSE)) {
-        console.log(`Round ${roundCounter}:Unfortunately this time ${LOOSE}`)
+        console.log(`Round ${roundCounter}: Unfortunately this time ${LOOSE}`)
         computerScore++;
     }
     else {
@@ -30,17 +48,18 @@ function scoreChecker() {
         return        
     }
 }
+//Criers show results to user
 function crierScores(){
     console.log(`Your score was: ${playerScore}, while computer was: ${computerScore}`)
 }
-//Logs in console the results of the game.
+
 function crierGameResults(){
     if (playerScore===5){
         console.log(`Rejoice mortal! You won in ${roundCounter} rounds!`)
         crierScores()
     }
     else if (computerScore===5) {
-        console.log(`The computer won. Neverlucky. The game lasted ${roundCounter}`)
+        console.log(`The computer won. Neverlucky. The game lasted ${roundCounter} rounds`)
         crierScores()
     }
     else {
@@ -48,17 +67,17 @@ function crierGameResults(){
         crierScores()
     }
 }
-//End of helper functions ^^^
+//End of helper functions ------------------
 
 //Plays a single round and return a string.
 function singleRound(playerPlay,computerPlay) {
+    playerValidate(playerPlay);
     let computerPlayLc=computerPlay();
-    let playerPlayLc = playerPlay.toLowerCase()
-    if (playerPlayLc === computerPlayLc) {
+    if (playerValidated === computerPlayLc) {
         check=TIE
     }
     else {
-        switch (playerPlayLc+computerPlayLc) {
+        switch (playerValidated+computerPlayLc) {
             case ("rockpaper"):
                 check=`Paper wraps Rock! ${LOOSE}`;
                 break;
@@ -91,4 +110,5 @@ function game(){
     scoreChecker(singleRound);
     }
     crierGameResults();
+    initialize();
 }
